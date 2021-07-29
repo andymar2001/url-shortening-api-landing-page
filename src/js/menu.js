efbdaef94c7ef58c34b028f055b5db6a0b0ea01c
Andy Alexis Mariños Rodriguez <andy_mar_rey@outlook.com>
2021-07-29 08:16:01 -0500

    avance de banner

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,17 @@
 const menu = document.getElementById("main-menu");
-const btnNav = document.getElementById("btn-nav");
+const btnMenu = document.getElementById("btn-nav");
+const body = document.getElementsByTagName("body")[0];
 
-btnNav.addEventListener("click", () => {
+btnMenu.addEventListener("click", () => {
   menu.classList.toggle("main-menu--active");
+});
+
+body.addEventListener("click", (e) => {
+  if (
+    e.target.parentElement.id != "btn-nav" &&
+    e.target.tagName != "path" &&
+    e.target.id != "main-menu"
+  ) {
+    menu.classList.remove("main-menu--active");
+  }
 });
